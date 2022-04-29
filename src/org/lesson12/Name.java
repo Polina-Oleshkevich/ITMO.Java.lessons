5a1b3df1574bbb970082785563e27790bfb19ffb
Polina-Oleshkevich <poli3108@yandex.ru>
2022-04-29 10:22:34 +0300

    lesson12

--- src/org/lesson12/Name.java
+++ src/org/lesson12/Name.java
@@ -1,23 +1,21 @@
 package org.lesson12;
 
 public class Name extends Thread {
-    private final Object lock = new Object();
+   // private final Object lock = new Object();
 
     @Override
-    public void run() {
-        synchronized (lock) {
-            lock.notifyAll();
-            try {
-                lock.wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            int i = 0;
-            while (i < 100) {
-                i++;
-
-                System.out.println(Thread.currentThread().getName());
-            }
+    public synchronized void run() {
+        notifyAll();
+        try {
+            wait();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        int i = 0;
+        while (i < 100) {
+            i++;
+            System.out.println(Thread.currentThread().getName());
         }
     }
 }
+
